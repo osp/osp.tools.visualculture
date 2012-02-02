91bb864855d1777d5d827385f29b7d6d4bb0c3c2
Eric Schrijver <eric@ericschrijver.nl>
2012-02-02 17:09:30 +0100

    First commit, run $ shotgun app.rb "path/to/git/repo"
    Based on https://gist.github.com/934376 by Stefano Verna
    
    Just changed that the repo is supplied as a command line argument.
    No licence was supplied but probably the code will change as much
    so that this original base will not be there anymore…

---
+++ README.txt
@@ -0,0 +1,12 @@
+OSP Visual Culture Git Viewer
+=============================
+
+- - -
+
+$ sudo gem install sinatra grit haml shotgun
+
+Use shotgun to serve with auto reload,
+specify git repo as argument
+
+$ shotgun app.rb "/home/e/osp/osp.work.panik"
+
---
+++ app.rb
@@ -0,0 +1,50 @@
+%w(sinatra grit).each { |gem| require gem }
+
+mime_type :binary, 'binary/octet-stream'
+set :repo, Grit::Repo.new(ARGV[1])
+
+before %r{^/(\w+)} do
+  commit_id = params[:captures].first[0..10]
+  @commit = settings.repo.commits(commit_id).first
+  halt "No commit exists with id #{commit_id}" if @commit.nil?
+end
+
+get "/" do
+  @commits = settings.repo.commits
+  haml :index
+end
+
+get "/:commit_id" do |commit_id|
+  @tree = @commit.tree
+  @path = ""
+  haml :dir
+end
+
+get "/:commit_id/*" do |commit_id, path|
+  @object = @commit.tree / path
+  halt "No object exists with path #{path}" if @object.nil?
+  if @object.is_a? Grit::Blob
+    content_type :binary
+    @object.data
+  else
+    @tree = @object
+    @path = path + "/"
+    haml :dir
+  end
+end
+
+__END__
+
+@@ index
+%ul
+  - @commits.each do |commit|
+    %li
+      %a{ :href => "/#{commit.id[0..10]}" }= "#{commit.id[0..10]} (by #{commit.author}, #{commit.committed_date})"
+
+@@ dir
+%h1= "Commit #{@commit.id[0..10]} - Path: #{@path}"
+%ul
+  - @tree.contents.each do |obj|
+    %li
+      %a{ :href => "/#{@commit.id}/#{@path}#{obj.name}" }= obj.name
+
