b7beeb571369cfb4e04010f1af13850367552bd4
Eric Schrijver <eric@ericschrijver.nl>
2012-05-31 17:02:01 +0200

    New transducers, a new function to create paths to the cache
    And I figured out the cache actually wasn’t working at all!
    That is because the compose_path function wasn’t being used
    inside the transducers. So they got saved somewhere else.
    
    It still isn’t working but we should be able to get it to work now.
    
    btw I deleted the module method to transduce because it was super
    confusing to have to maintain the same method twice

--- lib/vc-transduction.rb
+++ lib/vc-transduction.rb
@@ -1,27 +1,12 @@
+require 'fileutils'
+
 require 'transducers/base'
 require 'transducers/png'
 require 'transducers/jpg'
 require 'transducers/gif'
 require 'transducers/svg'
-
-module VC
-  def self.transduce(blob, s=nil)
-    if VC::Transducers.handlers[self.mime_type]
-      size = size.nil? ? VC.settings("preview-image-size") : size
-      if self.cached? commit, size
-        compose_path(commit, size)
-      else
-        VC::Transducers.handlers[self.mime_type].call(self, size)
-      end
-    else
-      nil
-    end
-  end
-
-  def self.transducer?(blob)
-    VC::Transducers.handlers[blob.mime_type] ? true : false
-  end  
-end
+require 'transducers/otf'
+require 'transducers/ttf'
 
 module VC
   module TransductionHelper
@@ -31,6 +16,7 @@ module VC
         if self.cached? commit, size
           compose_path(commit, size)
         else
+          FileUtils.mkpath File.dirname(self.compose_path_2(size))
           VC::Transducers.handlers[self.mime_type].call(self, size)
         end
       else
