cd4f7715cc816a901cc7a1f60fac233db05b40e8
Eric Schrijver <eric@ericschrijver.nl>
2012-05-31 15:41:45 +0200

    Remove commented out code for clarity

--- lib/vc-cache.rb
+++ lib/vc-cache.rb
@@ -6,23 +6,12 @@ module VC
     end
 
     def compose_path(commit, size=nil)
-#      commit = self.blame[0].id
       size = size.nil? ? VC.settings("thumb-image-size").to_i : size
       cache_commit_dir = VC.settings("cache-dir") + commit.id[0..10] 
-#      unless Dir.exist? cache_commit_dir + "/" + size.to_s
-#        unless Dir.exist? cache_commit_dir
-#          Dir.mkdir cache_commit_dir
-#        end
-#        Dir.mkdir cache_commit_dir + size.to_s
-#      end
       ext = VC::Transducers.extensions[self.mime_type] ? VC::Transducers.extensions[self.mime_type] : File.extname(self.name)
       new_name = File.basename(self.name, File.extname(self.name)) + ext
       cache_commit_dir + "/" + size.to_s + "/" + new_name
     end
-
-#    def cache(size=nil)
-#      self.transduce
-#    end
 
   end
 end
