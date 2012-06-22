87989d5c873f52ac84e26739d07083edcc8cced1
Eric Schrijver <eric@ericschrijver.nl>
2012-06-22 12:32:29 +0200

    Make the api aware of urls, allow more dots in slugs

--- lib/vc-repo.rb
+++ lib/vc-repo.rb
@@ -9,7 +9,11 @@ module VC
       end
     end
     def web_path
-      self.slug.split('.')[-2..-1].join('/')
+      components = self.slug.split('.')
+      [components[1], components[2..-1].join('.')].join('/')
+    end
+    def web_url
+      VC.settings("web-domain") + self.web_path + '/'
     end
     def category
       self.slug.split('.')[1]
@@ -54,13 +58,13 @@ module VC
       nil
     end
     def commit_overview
-      self.commits.map {|commit| { 'id' => commit.id[0..10], 'author' => commit.author, 'date' => commit.committed_date, 'date_iso' => commit.committed_date.xmlschema, 'message' => commit.message, 'message_short' => commit.message.lines.first }}
+      self.commits.map {|commit| { 'id' => commit.id[0..10], 'author' => commit.author, 'date' => commit.committed_date, 'date_iso' => commit.committed_date.xmlschema, 'message' => commit.message, 'message_short' => commit.message.lines.first, 'parent_repo_slug' => self.slug, 'parent_repo_title' => self.title, 'parent_repo_url' => self.web_url }}
     end
     def last_updated
       self.commit('HEAD').date.xmlschema
     end
     def to_hash
-      {'slug' => self.slug, 'web_path' => self.web_path, 'title' => self.title, 'project_description' => self.project_description, 'iceberg' => self.iceberg, 'commits' => self.commit_overview, 'blog_post' => self.blog_post, 'gallery' => self.gallery, 'source_files' => self.source_files, 'last_updated' => self.last_updated}
+      {'slug' => self.slug, 'web_path' => self.web_path, 'web_url' => self.web_url, 'title' => self.title, 'project_description' => self.project_description, 'iceberg' => self.iceberg, 'commits' => self.commit_overview, 'blog_post' => self.blog_post, 'gallery' => self.gallery, 'source_files' => self.source_files, 'last_updated' => self.last_updated}
     end
     def to_json
       self.to_hash.to_json
