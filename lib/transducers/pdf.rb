c1a16df513e7feef92bb510014ef120d72022e32
Pierre Marchand <pierre@oep-h.com>
2012-07-06 13:27:19 +0200

    Rewrite of PDF transducer. RMagick was using way too much resources to turn PDFs into images. By using Poppler, it seems that we can get faster rendering, the price being that the ruby binding comes with GTK dependecies.

--- lib/transducers/pdf.rb
+++ lib/transducers/pdf.rb
@@ -3,14 +3,28 @@
 $:.unshift File.join(File.dirname(__FILE__), "..")
 require 'visualculture'
 
+require 'cairo'
+require 'poppler'
+
 module VC
   module Transducers
+	  begin
+  require 'gtk2'
+rescue Gtk::InitError
+end
     pdf = lambda {|blob, size|
-      name = blob.compose_path_2(size)
-      r = Image.from_blob(blob.data)
-      img = r[0].resize_to_fit(size.to_i, size.to_i)
-      img.write name
-      name
+	name = blob.compose_path_2(size)
+	tmpf = File.join( VC.settings("cache-dir"), "#{blob.id[0..10]}.pdf")
+	File.open(tmpf, 'w') do |f|
+		f.write blob.data
+	end
+	surf = Cairo::ImageSurface.new(size.to_i,size.to_i)
+	ctx = Cairo::Context.new(surf)
+	doc = Poppler::Document.new(tmpf)
+	page = doc.get_page(0)
+	page.render(ctx)
+	surf.write_to_png(name)
+        name
     }    
     @handlers["application/pdf"] = pdf
     @extensions["application/pdf"] = '.png'
