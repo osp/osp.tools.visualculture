ab1226cd30b77b8ae934581cbd607e9c50720cdc
Pierre Marchand <pierre@oep-h.com>
2012-07-15 11:24:22 +0200

    Added docs listing and find entry point to disputio

--- bobo/disputio/routes.py
+++ bobo/disputio/routes.py
@@ -5,16 +5,22 @@ import webob
 import jinja2
 
 import pymongo as mongo
+from pymongo import objectid
 
 
 
-def datetimeformat(value):
-    return datetime.date.fromtimestamp(value).isoformat()
     
     
 template_env = jinja2.Environment(loader=jinja2.PackageLoader('disputio', 'templates'))
-template_env.filters['datetimeformat'] = datetimeformat
 
+
+def get_collection(name = None):
+	if name is None:
+		name = 'disputio'
+	cn = mongo.Connection()
+	db = cn.RCE
+	col = db[name]
+	return col
 
 @bobo.query('')
 def base(bobo_request):
@@ -33,9 +39,55 @@ def add(bobo_request):
 	doc = {}
 	for k in bobo_request.POST:
 		doc[k] = bobo_request.POST[k]
-	cn = mongo.Connection()
-	db = cn.RCE
-	col= db.disputio
+	
+	col = get_collection()
 	col.insert(doc)
 	return bobo.redirect('/')
+	
+	
+def r_to_doc(r, keys):
+	doc = {}
+	for k in keys:
+		if k == '_id':
+			doc[k] = str(r[k])
+		else:
+			if r.has_key(k):
+				doc[k] = r.get(k)
+			else:
+				doc[k] = ''
+	return doc
+	
+@bobo.query('/docs')
+def docs():
+	col = get_collection()
+	res = col.find()
+	ids = []
+	for r in res:
+		ids.append(str(r.get('_id')))
+		
+	template = template_env.get_template('docs.html')
+	return template.render({'ids':ids})
+
+@bobo.query('/:key/:value')
+def bykey(key, value):
+	col = get_collection()
+	pat = {key:value}
+	if key == 'id' or key == '_id':
+		pat = {'_id':objectid.ObjectId(value)}
+	res = col.find(pat)
+	keys_ = {}
+	for r in res:
+		for k in r.iterkeys():
+			keys_[k] = 1
+	keys = keys_.keys()
+	resource = []
+	res.rewind()
+	for r in res:
+		resource.append(r_to_doc(r, keys))
+		
+	print('KEYS: %s'%keys)
+	print('R: %s'%resource)
+		
+	template = template_env.get_template('find.html')
+	return template.render({'key':key, 'value':value, 'keys':keys, 'resource':resource})
 
