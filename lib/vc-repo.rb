89dcbf04f84a2007ef2b8ea1993c599194fe7157
Eric Schrijver <eric@ericschrijver.nl>
2012-08-17 21:41:58 +0200

    Comments explaining the methods we add to the Repository class
    Also, implement a fallback for a Title if there is no README

--- lib/vc-repo.rb
+++ lib/vc-repo.rb
@@ -2,24 +2,40 @@
 module VC
   module RepoHelper
     def slug
+      # use the path to the repo to find the slug:
       components = self.path.split('/')
       if components.last == ".git"
+        # checked out repo
+        # /home/o/osp/osp.work.bessst/.git(/) -> osp.work.bessst
         components[-2]
       else
+        # bare repo
+        # /home/o/osp/osp.work.bessst.git(/) -> osp.work.bessst
         components[-1].sub('.git','')
       end
     end
     def web_path
+      # 'osp.tools.visualculture' -> 'tools/visualculture'
+      # 'osp.tools.visualculture.test' -> 'tools/visualculture.test'
+      # 'osp.work.balsamine.2011-2012' -> 'work/balsamine.2011-2012'
+      # should think of some exception if there’s no dot
       components = self.slug.split('.')
       [components[1], components[2..-1].join('.')].join('/')
     end
     def web_url
+      # where to find this repo on the web:
+      # http://ospwork.constantvzw.org/work/balsamine.2011-2012/
       VC.settings("web-domain") + self.web_path + '/'
     end
     def category
+      # 'osp.tools.visualculture' -> 'tools'
+      # In ruby, this will return nil if there’s no dots in the slug
+      # should think of some exception in python
       self.slug.split('.')[1]
     end
     def _readme
+      # look at the root tree for a filename that looks like README
+      # and return it’s contents
       readme_file = self.tree.contents.map {|x| x.name.include?('README') ? x.name : nil}.compact.first
       f = (self.tree / readme_file)
       if f.nil?
@@ -33,16 +49,21 @@ module VC
       end
     end
     def title
+      # The title is usually the first line of the README
       if self._readme
         self._readme.lines.first.chomp
       else
-        "PLEASE ADD README.TXT"
+        # 'osp.tools.visualculture' -> 'visualculture'
+        self.slug.split('.').last
       end
     end
     def project_description
       if self._readme
-        # Take the introductory text from the README
-        # And convert it to html
+        # Returns the introductory text from the README
+        # converted to html
+        # (this pressuposes the readme is markdown,
+        # and that the introduction is demarcated from the rest by
+        # '- - -')
         m = RDiscount.new self._readme.split('- - -')[0]
         m.to_html
       else
@@ -53,21 +74,31 @@ module VC
       iceberg = (self.tree / "iceberg")
       if iceberg.nil?
         return nil
-      end      
+      end
+      # return all image files in the iceberg folder:
+      # (we could return other types of files as well, since you can access their rendered preview)
+      # (the ruby version had problems with symbolic links, by the way)
       iceberg.contents.map {|x| /jpg|jpeg|png|gif/i.match(x.name) ? x.name : nil}.compact
     end
     def source_files
-      # If there's only 2, that means just the README and the iceberg
-      # i.e. only presentation, not source files
+      # If there's only 2 items in the root tree,
+      # that likely means just the README file and the iceberg folder
+      # as is still the case with some of the old projects imported from Indexhibit
+      # in this case self.source_files = false
       self.tree.contents.length > 2
     end
     def blog_post
+      # I figured to scan the readme for links to blog posts
+      # not implemented
       nil
     end
     def gallery
+      # I figured to scan the readme for links to the gallery3
+      # not implemented
       nil
     end
     def commit_overview
+      # A hash of recent commits with elementary info, to be attached to the repo object in the api
       begin
         self.commits.map {|commit| { 'id' => commit.id[0..10], 'author' => commit.author.name.force_encoding("UTF-8"), 'date' => commit.committed_date, 'date_iso' => commit.committed_date.xmlschema, 'message' => commit.message.force_encoding("UTF-8"), 'message_short' => commit.message.lines.first.force_encoding("UTF-8"), 'parent_repo_slug' => self.slug, 'parent_repo_title' => self.title, 'parent_repo_url' => self.web_url }}
       rescue NoMethodError
@@ -75,9 +106,12 @@ module VC
       end 
     end
     def last_updated
+      # last updated in ISO 
       self.commit('HEAD').date.xmlschema
     end
     def recent
+      # Steph would like to know,
+      # has the repo been updated last month (approximately)? 
       if Time.now() - self.commit('HEAD').date < 30 * 24 * 60 * 60
         true
       else
@@ -85,6 +119,11 @@ module VC
       end
     end
     def html_classes
+      # inspired by P.M.’s suggestion to add plenty of classes that can be useful
+      # when styling…
+      # this loops over a number of properties of the repo, and if they turn
+      # out to be true, adds them to a list of space separated classes that
+      # can be easily used in an HTML template
       html_classes = []
       methods = ['iceberg', 'blog_post', 'gallery', 'source_files', 'recent']
       methods.each do |m|
@@ -96,6 +135,7 @@ module VC
       html_classes.join(' ')
     end
     def to_hash
+      # create a hash to serialise as a JSON object for the api
       {'slug' => self.slug, 'web_path' => self.web_path, 'web_url' => self.web_url, 'title' => self.title, 'project_description' => self.project_description, 'iceberg' => self.iceberg, 'commits' => self.commit_overview, 'blog_post' => self.blog_post, 'gallery' => self.gallery, 'source_files' => self.source_files, 'html_classes' => self.html_classes, 'last_updated' => self.last_updated}
     end
     def to_json
