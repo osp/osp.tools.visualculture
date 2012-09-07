8663e96e945aabe6a95938745640d7e859cd5582
Eric Schrijver <eric@ericschrijver.nl>
2012-09-07 23:50:09 +0200

    The part of the app that returns JSON is now namespaced as '/api/'

--- vc_django/urls.py
+++ vc_django/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    (r'^repos/', include('git_info.urls')),
+    (r'^api/', include('git_info.urls')),
     (r'^vc/', include('visual_culture.urls')),
     (r'^test/', include('test_browser.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # serve media in debug
