565a0b815b38a47d95f36670761929d379da6b83
Pierre Marchand <pierre@oep-h.com>
2012-10-09 17:35:19 +0200

    Add cache info to the resulting dictionary returned by the api when requesting a blob

--- vc_django/vc_cache/utils.py
+++ vc_django/vc_cache/utils.py
@@ -6,15 +6,26 @@ import os
 import errno
 
 def ensure_dir(path):
-	try:
-		os.makedirs(path)
-	except OSError as exception:
-		if exception.errno != errno.EEXIST:
-			raise exception
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise exception
 
 def hash_options(options):
-	opt_a = []
-	for k in options:
-		opt_a.append('%s..%s'%(k, options[k].replace('/','_')))
-	h = '/'.join(sorted(opt_a));
-	return h
+    opt_a = []
+    for k in options:
+        opt_a.append('%s..%s'%(k, options[k].replace('/','_')))
+    h = '/'.join(sorted(opt_a));
+    return h
+	
+def de_hash_options(options):
+    ret = {}
+    blocks = options.split('/')
+    for b in blocks:
+        kv = b.split('..')
+        try:
+            ret[kv[0]] = kv[1]
+        except Exception:
+            pass
+    return ret
