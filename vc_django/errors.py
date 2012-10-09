b541676a8b53f82768a4898b36c41d2e729e219c
Pierre Marchand <pierre@oep-h.com>
2012-10-09 15:44:26 +0200

    commited wrong file

--- vc_django/errors.py
+++ vc_django/errors.py
@@ -2,10 +2,11 @@ import traceback
 import sys
 from django.http import HttpRequest, HttpResponse
         
-def error_500(req):
+def error_500():
     etype, value, tb = sys.exc_info()
     e_txt = []
     e_txt.append('<h1>%s : %s</h1>'%(etype.__name__, value))
-    for t in traceback.format_tb(tb):
+    tb_fmt = traceback.format_tb(tb)
+    for t in tb_fmt:
         e_txt.append('<div style="font-family:monospace">%s</div>'%(t,))
     return HttpResponse('\n'.join(e_txt))
