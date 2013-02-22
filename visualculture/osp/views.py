b05a4615754fdcfe068b3a75a5eb8da0cc1710f8
Eric Schrijver <eric@ericschrijver.nl>
2013-02-22 15:44:03 +0100

    As Steph made as see the whole story on the project page, we better begin from the beginning.
    (+ fix broken commit spacing on project page )
    (+ sorry, fixed regression, moved back suncalc to it’s proper place )

--- visualculture/osp/views.py
+++ visualculture/osp/views.py
@@ -106,19 +106,19 @@ def browse(request, category, name, path):
     commits = []
     ellipse = 0
     i = 0
-    for commit in repo['commits']: # default is newest first, we start from the beginning
+    for commit in reversed(repo['commits']): # default is newest first, we start from the beginning
         c = commit
         
         if i != 0:
             commit_time=  datetime.fromtimestamp(c['commit_time'])
-            ellipse = float((previous_commit - c['commit_time']))/(24*60*60)
+            ellipse = float((c['commit_time'] - previous_commit))/(24*60*60)
             ellipse = log1p(ellipse) * 50
         i += 1
         previous_commit = c['commit_time']
         c['commit_time'] = datetime.fromtimestamp(c['commit_time'])
         c['ellipse'] = ellipse
         commits.append(c)
-            
+    repo['commits'] = commits
     
     # Render README
     
